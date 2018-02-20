36ed1de5e663f04f81e8618e8116ced5a2420d0d
silvia16 <36601499+silvia16@users.noreply.github.com>
2018-02-20 09:57:51 -0500

    Add files via upload

---
+++ new_sale_ex.py
@@ -0,0 +1,12 @@
+#Calculate the original price
+#Calculate the discount
+#Calculate the new sale price
+
+original_price = float (input ("Enter the original price "))
+discount = 0.20 * original_price 
+final_price= original_price - discount
+
+print ( "the original price is ", format (original_price,'.2f'))
+print ("the discount is " , format(discount,'.0f'))
+print ("the final price is " ,final_price)
+
