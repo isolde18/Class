ebae1c0237db6a96f48595af711ff408f16582a1
silvia16 <36601499+silvia16@users.noreply.github.com>
2018-02-20 10:09:50 -0500

    Add files via upload

---
+++ test_scores.py
@@ -0,0 +1,13 @@
+#CTI
+#20.02.2018
+#Silvia
+
+score1 = float (input ("Enter the first score "))
+score2 = float ( input ("Enter the second score "))
+score3 = float ( input ("Enter the third score "))
+
+average_score= (score1 + score2 + score3)/3
+
+print("The average of the three scores is " , average_score)
+               
+               
