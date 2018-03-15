9e1811d9353317174ca922f8613a69bf7add74fc
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-03-15 10:07:31 -0400

    Add files via upload

---
+++ for_in_range.py
@@ -0,0 +1,10 @@
+
+
+
+print("Num\ t Sqr")
+print("-----------")
+for num in range (1,10,2):
+
+    square=num**2
+
+    print(num,'\t',square)
---
+++ while_temp.py
@@ -0,0 +1,11 @@
+
+temp =int(input("Enter temprature:"))
+
+while temp>=100:
+    print("Temprature is too high!,'\n'"\
+        ("Turn thermo down and take temprature again in 5 min.")
+          
+temp = int(input("Enter the new temprature:,temp "))
+
+print("Temprature is accessible")          
+          
