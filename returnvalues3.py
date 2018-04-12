5c4bd50b09191c47fa8dc4a21a3df4f744a4b2d4
isolde18 <36601499+isolde18@users.noreply.github.com>
2018-04-12 10:26:52 -0400

    Add files via upload

---
+++ returnvalues3.py
@@ -0,0 +1,11 @@
+
+
+def number():
+    num1=int(input("Please enter number "))
+    num2=int(input("Please enter number2 "))
+
+    return num1,num2
+
+number1 , number2=number()
+print(number1)
+print(number2)
